89c64c7074170fde9d9b9576d76190f8e252635d
System Administrator <root@192.168.0.105>
2022-10-10 16:45:39 -0300

    first commit

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+  'password' => 'As senhas devem ter pelo menos seis caracteres e serem iguais a confirmação de senha.',
+  'reset' => 'Sua senha foi alterada!',
+  'sent' => 'Enviamos o link de redefinição de senha por e-mail!',
+  'token' => 'Este token de redefinição de senha é inválido.',
+  'user' => 'Não conseguimos encontrar um usuário com esse endereço de e-mail.',
+);
---
+++ resources/lang/en/sms.php
@@ -0,0 +1,6 @@
+<?php
+
+return array (
+  'generic-otp' => 'Seu OTP para :mobile é :otp. Não compartilhe com ninguém.',
+  'password-reset-otp' => 'Sua OTP de redefinição de senha é :otp.  Não compartilhe com ninguém.',
+);
